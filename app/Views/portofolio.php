052924bf2595993ee0e067916dae6c84fbde82a4
ubaidillah <ubayd2947@gmail.com>
2024-10-06 22:06:40 +0700

    update navbar

--- app/Views/portofolio.php
+++ app/Views/portofolio.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="logo.svg" type="image/x-icon">
 
+    <!-- icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -20,28 +23,41 @@
 
 <body>
     <!-- navbar start -->
-    <nav class="navbar bg-nav">
-        <div class="container">
-            <a class="navbar-brand nav-brand">
-                <svg width="23" height="23" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9.07107 19.799L18.2635 10.6066" stroke="#ffffffde" stroke-width="3"
-                        stroke-linecap="round" />
-                    <path d="M2 12.7279L7.65685 7.07107" stroke="#ffffffde" stroke-width="3" stroke-linecap="round" />
-                    <path
-                        d="M18.2635 3.53553C18.7277 3.99983 19.096 4.55102 19.3473 5.15765C19.5986 5.76428 19.7279 6.41446 19.7279 7.07107C19.7279 7.72768 19.5986 8.37786 19.3473 8.98448C19.096 9.59111 18.7277 10.1423 18.2635 10.6066C17.7992 11.0709 17.248 11.4392 16.6413 11.6905C16.0347 11.9417 15.3845 12.0711 14.7279 12.0711C14.0713 12.0711 13.4211 11.9417 12.8145 11.6905C12.2079 11.4392 11.6567 11.0709 11.1924 10.6066"
-                        stroke="#ffffffde" stroke-width="3" stroke-linecap="round" />
-                    <path
-                        d="M14.7279 7.07107C14.2636 6.60677 13.7124 6.23848 13.1058 5.9872C12.4992 5.73593 11.849 5.6066 11.1924 5.6066C10.5358 5.6066 9.8856 5.73593 9.27897 5.9872C8.67234 6.23848 8.12115 6.60678 7.65685 7.07107C7.19256 7.53536 6.82426 8.08656 6.57299 8.69319C6.32172 9.29981 6.19239 9.94999 6.19239 10.6066C6.19239 11.2632 6.32172 11.9134 6.57299 12.52C6.82426 13.1266 7.19256 13.6778 7.65685 14.1421"
-                        stroke="#ffffffde" stroke-width="3" stroke-linecap="round" />
-                </svg>
-                Navbar</a>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+    <nav class="navbar bg-nav pt-4 pb-3">
+        <div class="container d-flex">
+            <div class="pe-5">
+                <a href="#" class="navbar-brand d-flex align-items-center">
+                    <svg class=" me-2" width="27" height="27" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9.07107 19.799L18.2635 10.6066" stroke="#ffffffde" stroke-width="3"
+                            stroke-linecap="round" />
+                        <path d="M2 12.7279L7.65685 7.07107" stroke="#ffffffde" stroke-width="3" stroke-linecap="round" />
+                        <path
+                            d="M18.2635 3.53553C18.7277 3.99983 19.096 4.55102 19.3473 5.15765C19.5986 5.76428 19.7279 6.41446 19.7279 7.07107C19.7279 7.72768 19.5986 8.37786 19.3473 8.98448C19.096 9.59111 18.7277 10.1423 18.2635 10.6066C17.7992 11.0709 17.248 11.4392 16.6413 11.6905C16.0347 11.9417 15.3845 12.0711 14.7279 12.0711C14.0713 12.0711 13.4211 11.9417 12.8145 11.6905C12.2079 11.4392 11.6567 11.0709 11.1924 10.6066"
+                            stroke="#ffffffde" stroke-width="3" stroke-linecap="round" />
+                        <path
+                            d="M14.7279 7.07107C14.2636 6.60677 13.7124 6.23848 13.1058 5.9872C12.4992 5.73593 11.849 5.6066 11.1924 5.6066C10.5358 5.6066 9.8856 5.73593 9.27897 5.9872C8.67234 6.23848 8.12115 6.60678 7.65685 7.07107C7.19256 7.53536 6.82426 8.08656 6.57299 8.69319C6.32172 9.29981 6.19239 9.94999 6.19239 10.6066C6.19239 11.2632 6.32172 11.9134 6.57299 12.52C6.82426 13.1266 7.19256 13.6778 7.65685 14.1421"
+                            stroke="#ffffffde" stroke-width="3" stroke-linecap="round" />
+                    </svg>
+                    Ubaidillah
+                </a>
+            </div>
+            <div class="link d-flex justify-content-start flex-fill">
+                <a class="px-3 link-underline link-underline-opacity-0" href="#home">Home</a>
+                <a class="px-3 link-underline link-underline-opacity-0" href="#portofolio">Portofolio</a>
+                <a class="px-3 link-underline link-underline-opacity-0" href="#about">About</a>
+            </div>
+            <div class="socials d-flex justify-content-end">
+                <a class="px-2" href=""><i class="feather" data-feather="dribbble"></i></a>
+                <a class="px-2" href=""><i class="feather" data-feather="linkedin"></i></a>
+                <a class="px-2" href=""><i class="feather" data-feather="align-right"></i></a>
+            </div>
         </div>
     </nav>
     <!-- navbar end -->
+
+    <script>
+        feather.replace();
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
